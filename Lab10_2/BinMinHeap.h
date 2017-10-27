2787d50b8be017aea1e9bead5a643f6611c97f69
Zulcom <s01@ngs.ru>
2017-10-27 23:34:08 +0700

    Lab10_2

---
+++ Lab10_2/BinMinHeap.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "vector"
+using namespace std;
+
+class BinMinHeap
+{
+private:
+	vector<int> _vector;
+	void BubbleDown(int index);
+	void BubbleUp(int index);
+	void Heapify();
+
+public:
+	BinMinHeap(int* array, int length);
+	BinMinHeap(const vector<int>& vector);
+	BinMinHeap();
+	void remove(int index);
+	int findItem(int value);
+	void Insert(int newValue);
+	int GetMin();
+	void DeleteMin();
+	void Display();
+	int getSize();
+	vector<int> BinMinHeap::extractLevel(int level);
+};
--- Lab10_2/Lab10_2.cpp
+++ Lab10_2/Lab10_2.cpp
@@ -1,26 +1,31 @@
 ﻿/*
-2. Осуществить алгоритм упорядоченного дерева поиска. Найти ключ поиска и удалить его из дерева.
-*/
+ * 2. Осуществить алгоритм упорядоченного дерева поиска. Найти ключ поиска и удалить его из дерева.
+ * Делаем бинарное дерево поиска с минимумом наверху.
+ */
+#include "BinMinHeap.h"
 #include <iostream>
 #include <vector>
-using namespace  std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int n, key;
-	std::cout << "Enter n and key:";
-	cin >> n >> key;
-	vector<int> arr;
-	int counter = 0;
-	for(int i = 0; i < n; i++)
-	{
-		arr.push_back(rand() % 10);
-		cout << arr.at(i) << " ";
-		if(arr.at(i) == key) counter++;
-	}
-	cout << endl << "Количество совпадений:" << counter << endl;
-	cout << "Номера сопадений:" << endl;
-	for(int i = 0; i < n; i++) { if(arr.at(i) == key) cout << i + 1 << " "; }
+	
+	int array2[] = {117, 42, 56, 29, 37, 50,48,2,15,1,12};
+
+	
+	BinMinHeap bin_heap(array2, 11);
+	
+	cout << " binary heap: " << endl;
+	bin_heap.Display();
+
+	cout << "Enter value to remove from heap:" << endl;
+	int remove;cin >> remove;
 	cout << endl;
+	int pos = bin_heap.findItem(remove);
+	if(pos == 0) cout << "Element not found."; 
+	else bin_heap.remove(pos);
+	cout << "binary heap after remove:" << endl;
+	bin_heap.Display();
+	cout << endl;
+
 	system("pause.exe");
+	return 0;
 }
