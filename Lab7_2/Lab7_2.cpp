cc1002c9ac0dd5dbee610a2325e2dab3ac90f995
Zulcom <s01@ngs.ru>
2017-10-27 19:51:57 +0700

    Lab 7.3

--- Lab7_2/Lab7_2.cpp
+++ Lab7_2/Lab7_2.cpp
@@ -1,20 +1,26 @@
 ﻿/*
- * 2.	Протабулировать функцию y= ex+cos x для  -1<=x<=1.5 с шагом 0.1,  записать данные в файл и просмотреть данные из файла.
+ * 2.	Протабулировать функцию y= e^x+cos(x) для  -1<=x<=1.5 с шагом 0.1,  записать данные в файл и просмотреть данные из файла.
  */
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace  std;
 int main()
 {
-	ofstream out("in.txt");
-	double sum = 0, temp = 0;
-	int counter = 0;
-	while(in >> temp)
+	ofstream out("out.txt");
+	out << "x   y(x)" << endl;
+	for (double x=-1;x<=1.6;x= x+0.1)
 	{
-		sum += temp;
-		counter++;
+		if(x == -1.3877787807814457e-16) x = 0; // Компилятор искренне считает, что -0.1+ 0.1 = -1.3877787807814457e-16
+		out << x << " "<< exp(x) + cos(x) <<endl;
 	}
-	cout << "Sum: " << sum << " AVG:" << sum / counter << endl;
+	out.close();
+	string temp;
+	ifstream in("out.txt");
+	while(getline(in,temp))
+		cout << temp <<endl;
+	in.close();
 	system("pause.exe");
 	return 0;
 }
---
+++ Lab7_3/Source.cpp
@@ -0,0 +1,34 @@
+﻿/*
+* 3.	Создать файл, содержащий данные: ФИО работника;  год рождения; стаж работы.  Просмотреть данные из файла. Определить фамилию работника, имеющего самый большой стаж работы.
+*/
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+using namespace  std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string temp,maxF;
+	int max = 0;
+	ifstream in("in.txt");
+	while(getline(in, temp))
+	{
+		cout << temp << endl;
+		istringstream iss(temp);
+		vector<string> tokens{istream_iterator<string>{iss},
+			istream_iterator<string>{}};
+		if(stoi(tokens.at(4)) > max)
+		{
+			max = stoi(tokens.at(4));
+			maxF = tokens.at(0);
+		}
+	}
+	in.close();
+	cout << "Максимальный стаж " << max << " найден у работника по фамилии " << maxF<<endl;
+	
+	system("pause.exe");
+	return 0;
+}
