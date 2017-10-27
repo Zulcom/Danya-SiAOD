86326a26911103cf7dc6eccddecf259573a16194
Zulcom <s01@ngs.ru>
2017-10-28 02:37:04 +0700

    Lab17 6-7

---
+++ Lab17_6/Lab17_6.cpp
@@ -0,0 +1,62 @@
+﻿/*
+
+Задания:
+Дан массив, состоящий из N целых случайных чисел. Отсортировать массив по возрастанию различными методами и оценить эффективность этих методов.
+Методы:
+• Быстрая сортировка (Хоара)
+
+
+*/
+#include <iostream>
+#include <cassert>
+#include <ctime>
+using namespace std;
+
+void quickSortR(int* a, int N)
+{
+
+	long i = 0, j = N;      // поставить указатели на исходные места
+	int  p;
+	p = a[N >> 1];      // центральный элемент
+						// процедура разделения
+	do
+	{
+		while(a[i] < p) i++;
+		while(a[j] > p) j--;
+
+		if(i <= j)
+		{	
+			swap(a[i], a[j]);
+			i++; j--;
+		}
+	} while(i <= j);
+
+	// рекурсивные вызовы, если есть, что сортировать 
+	if(j > 0) quickSortR(a, j);
+	if(N > i) quickSortR(a + i, N - i);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int n;
+	cout << "Input n:";
+	cin >> n;
+	int *m = new int[n];
+	cout << "Несортированный массив:" << endl;
+	for(int i = 0; i < n; i++)
+	{
+		m[i] = rand() % 10;
+		cout << m[i] << " ";
+	}
+	cout << endl;
+	quickSortR(m, n-1);
+	cout << "сортированный массив:" << endl;
+	for(int i = 0; i < n; i++)
+		cout << m[i] << " ";
+	cout << endl;
+	delete[] m;
+	system("pause.exe");
+	return 0;
+}
---
+++ Lab17_7/Lab17_7.cpp
@@ -0,0 +1,78 @@
+﻿/*
+
+Задания:
+Дан массив, состоящий из N целых случайных чисел. Отсортировать массив по возрастанию различными методами и оценить эффективность этих методов.
+Методы:
+• 	Сортировка слиянием
+
+
+*/
+#include <iostream>
+#include <cassert>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+static void merge(int * buf, size_t left, size_t right, size_t middle)
+{
+	if(left >= right || middle < left || middle > right) return;
+	if(right == left + 1 && buf[left] > buf[right])
+	{
+		swap(buf[left], buf[right]);
+		return;
+	}
+
+	vector<int> tmp(&buf[left], &buf[left] + (right + 1));
+
+	for(size_t i = left, j = 0, k = middle - left + 1; i <= right; ++i)
+	{
+		if(j > middle - left)
+		{
+			buf[i] = tmp[k++];
+		}
+		else if(k > right - left)
+		{
+			buf[i] = tmp[j++];
+		}
+		else
+		{
+			buf[i] = (tmp[j] < tmp[k]) ? tmp[j++] : tmp[k++];
+		}
+	}
+}
+void MergeSort(int * buf, size_t left, size_t right)
+{
+	//! Условие выхода из рекурсии
+	if(left >= right) return;
+
+	size_t middle = left + (right - left) / 2;
+
+	//! Рекурсивная сортировка полученных массивов
+	MergeSort(buf, left, middle);
+	MergeSort(buf, middle + 1, right);
+	merge(buf, left, right, middle);
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int n;
+	cout << "Input n:";
+	cin >> n;
+	int *m = new int[n];
+	cout << "Несортированный массив:" << endl;
+	for(int i = 0; i < n; i++)
+	{
+		m[i] = rand() % 10;
+		cout << m[i] << " ";
+	}
+	cout << endl;
+	MergeSort(m,0, n-1);
+	cout << "сортированный массив:" << endl;
+	for(int i = 0; i < n; i++)
+		cout << m[i] << " ";
+	cout << endl;
+	delete[] m;
+	system("pause.exe");
+	return 0;
+}
