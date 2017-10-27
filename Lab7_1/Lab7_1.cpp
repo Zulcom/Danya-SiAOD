9304862aa24ca4f3211bc15a7e69067fa650e82c
Zulcom <s01@ngs.ru>
2017-10-27 19:01:28 +0700

    Lab7_1

---
+++ Lab7_1/Lab7_1.cpp
@@ -0,0 +1,19 @@
+﻿/*
+ * 1.	Создать файл, содержащий вещественные числа. Найти сумму и среднее арифметическое этих чисел.
+ */
+#include <fstream>
+#include <iostream>
+using namespace  std;
+int main() {
+	ifstream in("in.txt");
+	double sum = 0, temp = 0;
+	int counter = 0;
+	while(in >> temp)
+	{
+		sum += temp;
+		counter++;
+	}
+	cout << "Sum: " << sum << " AVG:" << sum / counter <<endl;
+	system("pause.exe");
+	return 0;
+}
