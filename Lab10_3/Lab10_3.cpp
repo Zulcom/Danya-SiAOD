de8f28c86293310b0eba66f73ab48cb90cb33d0b
Zulcom <s01@ngs.ru>
2017-10-27 23:46:26 +0700

    Lab10_3

---
+++ Lab10_3/Lab10_3.cpp
@@ -0,0 +1,21 @@
+﻿/*
+ * 3. В текстовом файле хранится текст. Осуществить прямой поиск введенного пользователем слова.
+ */
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ifstream in("in.txt");
+	string find;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите слово:";
+	getline(cin, find);
+	string temp;
+	while(in >> temp)
+		if(temp == find) cout << "Слово найдено. ";
+	in.close();
+	system("pause.exe");
+	return 0;
+}
